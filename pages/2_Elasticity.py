599e76691f1a4b68be72b713c1402af89d091dee
EugenioABA <58227437+EugenioABA@users.noreply.github.com>
2023-11-10 08:53:22 -0600

    Update 2_Elasticity.py
    adding curve section

--- pages/2_Elasticity.py
+++ pages/2_Elasticity.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 #add ABA logo
@@ -92,3 +96,75 @@ plt.xlabel("Membership Price ($)") # add the x axis label
 plt.ylabel("Membership Demand") # add the y axis label
 plt.title("Price Elasticity of Demand") # add the title # show the plot
 st.pyplot() # show the plot in streamlit
+
+
+####################################################################################################################################
+####    Curves
+####################################################################################################################################
+
+# Calculate the price elasticity of demand
+price_elasticity_of_demand = (quantity_2 - quantity_1) / ((quantity_2 + quantity_1) / 2) / (price_2 - price_1) * (price_2 + price_1) / 2
+
+# Calculate the slope and intercept of the demand line
+slope = (quantity_2 - quantity_1) / (price_2 - price_1)
+intercept = quantity_1 - (slope * price_1)
+
+# Calculate the price at which revenue is maximized
+optimal_price = -intercept / (2 * slope)
+
+# Generate prices from 0 to just beyond the price that gives zero demand
+max_price_for_demand = -intercept / slope
+extended_prices = np.linspace(0, max_price_for_demand, 1000)
+
+# Calculate the demand and revenue for the extended price range
+extended_demands = slope * extended_prices + intercept
+extended_revenues = extended_prices * extended_demands
+
+# Plot the demand and revenue curves
+plt.figure(figsize=(14, 20))
+
+# Demand curve plot
+ax1 = plt.subplot(2, 1, 1)
+ax1.plot(extended_prices, extended_demands, label='Demand Curve')
+ax1.scatter([price_1, price_2], [quantity_1, quantity_2], color='red', zorder=5)
+ax1.axvline(x=optimal_price, color='red', linestyle='--')
+ax1.axvline(x=price_1, color='grey', linestyle='--')
+ax1.axvline(x=price_2, color='grey', linestyle='--')
+ax1.text(price_1, quantity_1, f'  Q1 = {quantity_1}', verticalalignment='bottom', horizontalalignment='right')
+ax1.text(price_2, quantity_2, f'  Q2 = {quantity_2}', verticalalignment='bottom', horizontalalignment='right')
+ax1.xaxis.set_major_locator(MaxNLocator(integer=True, nbins=25))
+ax1.yaxis.set_major_locator(MaxNLocator(integer=True, nbins=25))
+ax1.set_title('Demand Curve')
+ax1.set_xlabel('Price')
+ax1.set_ylabel('Quantity Demanded')
+ax1.grid(True)
+ax1.legend()
+
+# Revenue curve plot
+ax2 = plt.subplot(2, 1, 2, sharex=ax1)
+ax2.plot(extended_prices, extended_revenues, label='Revenue Curve', color='green')
+ax2.scatter([price_1, price_2, optimal_price], [price_1 * quantity_1, price_2 * quantity_2, optimal_price * (slope * optimal_price + intercept)], color='red', zorder=5)
+ax2.axvline(x=optimal_price, color='red', linestyle='--')
+ax2.axvline(x=price_1, color='grey', linestyle='--')
+ax2.axvline(x=price_2, color='grey', linestyle='--')
+ax2.text(price_1, price_1 * quantity_1, f'  TR1 = ${price_1 * quantity_1:.2f}', verticalalignment='bottom', horizontalalignment='right')
+ax2.text(price_2, price_2 * quantity_2, f'  TR2 = ${price_2 * quantity_2:.2f}', verticalalignment='bottom', horizontalalignment='right')
+ax2.text(optimal_price, optimal_price * (slope * optimal_price + intercept), f'  Max TR = ${optimal_price * (slope * optimal_price + intercept):.2f}', verticalalignment='bottom', horizontalalignment='left')
+ax2.get_yaxis().set_major_formatter(FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+ax2.xaxis.set_major_locator(MaxNLocator(integer=True, nbins=25))
+ax2.yaxis.set_major_locator(MaxNLocator(integer=True, nbins=25))
+ax2.set_title('Revenue Curve')
+ax2.set_xlabel('Price')
+ax2.set_ylabel('Total Revenue')
+ax2.grid(True)
+ax2.legend()
+
+plt.tight_layout()
+#plt.show()
+st.pyplot()
+
+# Output the calculated values
+st.info(f"Price Elasticity of Demand: {price_elasticity_of_demand}")
+st.info(f"Slope: {slope}")
+st.info(f"Intercept: {intercept}")
+
